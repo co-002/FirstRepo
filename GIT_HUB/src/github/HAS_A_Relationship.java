dfff208c7d0c8737131d237d4ed416a0f71be8f0
Admin <Admin@DESKTOP-I8IMRFN>
2023-04-07 10:56:05 +0530

    Changes in main file

--- GIT_HUB/src/github/HAS_A_Relationship.java
+++ GIT_HUB/src/github/HAS_A_Relationship.java
@@ -15,8 +15,24 @@ class Student{
 	}
 }
 
+class Teacher{
+	String name;
+	String subject;
+	
+	void setValues(String name, String subject) {
+		this.name = name;
+		this.subject = subject;
+	}
+	
+	void showValues() {
+		System.out.println("Teacher name : "+name);
+		System.out.println("Teacher subject : "+subject);
+	}
+}
+
 class School{
 	Student s = new Student();
+	Teacher t = new Teacher();
 }
 
 public class HAS_A_Relationship {
@@ -25,6 +41,7 @@ public class HAS_A_Relationship {
 		School s2 = new School();
 		School s3 = new School();
 		
+		// Calling student properties
 		s1.s.setValues(101, "Aniket");
 		s2.s.setValues(102, "Ram");
 		s3.s.setValues(103, "Shyam");
@@ -32,5 +49,12 @@ public class HAS_A_Relationship {
 		s1.s.displayStudentInfo();
 		s2.s.displayStudentInfo();
 		s3.s.displayStudentInfo();
+		
+		// Calling teacher properties
+		s1.t.setValues("Binita", "SIC");
+		s2.t.setValues("Nessi", "CL");
+		
+		s1.t.showValues();
+		s2.t.showValues();
 	}
 }
