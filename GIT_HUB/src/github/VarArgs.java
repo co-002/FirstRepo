b3fb710fbeec26f8dd004f530c810e4e6f206c5a
Admin <Admin@DESKTOP-I8IMRFN>
2023-04-07 11:03:03 +0530

    New functionality added

---
+++ GIT_HUB/src/github/VarArgs.java
@@ -0,0 +1,16 @@
+package github;
+
+class Example{
+	void fun(String...values) {
+		for(String val : values) {
+			System.out.println(val);
+		}
+	}
+}
+
+public class VarArgs {
+	public static void main(String[] args) {
+		Example e = new Example();
+		e.fun("A","B","C","D");
+	}
+}
